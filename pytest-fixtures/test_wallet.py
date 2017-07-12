8265ebfd03a97306528802f58548528b78e68b9d
Lee Chun On <jeffreyleeon@gmail.com>
2017-07-12 14:16:00 -0700

    Add test fixture and parametrized test function

--- pytest-fixtures/test_wallet.py
+++ pytest-fixtures/test_wallet.py
@@ -30,3 +30,17 @@ def test_wallet_spend_cash(wallet):
 def test_wallet_spend_cash_raises_exception_on_insufficient_amount(empty_wallet):
     with pytest.raises(InsufficientAmount):
         empty_wallet.spend_cash(100)
+
+# Parameterized test function
+@pytest.fixture
+def my_wallet():
+    return Wallet()
+
+@pytest.mark.parametrize("earned,spent,expected", [
+    (30, 10, 20),
+    (20, 2, 18),
+])
+def test_transactions(my_wallet, earned, spent, expected):
+    my_wallet.add_cash(earned)
+    my_wallet.spend_cash(spent)
+    assert my_wallet.balance == expected
